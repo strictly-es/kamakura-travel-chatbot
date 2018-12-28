b09c82587422ea369def5819feea6c79c1c5d977
Omura Shunsuke <omurashunsuke@Omuras-MacBook-Pro.local>
2018-12-28 20:50:38 +0900

    my second commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Header from "./Header";
-import Shop from "./shop/Shop";
 import Chatbot from "./chatbot/Chatbot.js";
 
 const App = () => {
--- index.js
+++ index.js
@@ -6,13 +6,14 @@ app.use(bodyParser.json());
 require("./routes/dialogFlowRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
+  // js and css files
   app.use(express.static("client/build"));
 
+  // index.html for all page routes
   const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
